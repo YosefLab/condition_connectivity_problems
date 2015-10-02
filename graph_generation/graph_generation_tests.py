88cda59584e70dde36b954d3ec945c482b8eb6ff
Jimmy <jimmywu126@gmail.com>
2015-10-02 02:05:06 -0700

    Restructured graph generation test.

--- graph_generation/graph_generation_tests.py
+++ graph_generation/graph_generation_tests.py
@@ -1,5 +1,5 @@
 """
-This file tests functionality in other modules.
+This file tests graph generation functionality.
 """
 from graph_generation import *
 import matplotlib.pyplot as pyplot
@@ -10,25 +10,25 @@ def test_create_sample_DCP_instance():
 
 	print("Graph has total weight " + str(graph.size()))
 
+	visualize_DCP_instance(graph, existence_for_node_time, connectivity_demands)
+
+
+
+
+def visualize_DCP_instance(graph, existence_for_node_time, connectivity_demands):
 	# Assign non-terminals red, terminals green
 	color_for_node = {node : 'r' for node in graph.nodes()}
 	for source, target, time in connectivity_demands:
 		color_for_node[source] = 'g'
 		color_for_node[target] = 'g'
 
-	visualize_graph(graph, color_for_node)
-
-
-
-
-def visualize_graph(G, color_for_node):
 	node_sequence = []
 	color_sequence = []
-	for node, color in color_for_node.items():
+	for node, color in color_for_node.iteritems():
 		node_sequence += [node]
 		color_sequence += [color]
 
-	networkx.draw(G, nodelist=node_sequence, node_color=color_sequence)
+	networkx.draw(graph, nodelist=node_sequence, node_color=color_sequence)
 
 	# Finer control over spring layout
 	# networkx.draw(G, pos=networkx.spring_layout(G, iterations=20), nodelist=node_sequence, node_color=color_sequence)
