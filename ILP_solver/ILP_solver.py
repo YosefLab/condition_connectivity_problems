ca81a21d5de8ad90360451dba57dc2d661d2a8db
Jimmy <jimmywu126@gmail.com>
2015-10-03 23:49:10 -0700

    Added DCP -> sDCP reduction framework.

--- ILP_solver/ILP_solver.py
+++ ILP_solver/ILP_solver.py
@@ -1,13 +1,59 @@
 from gurobipy import *
 
+
 def solve_DCP_instance(graph, existence_for_node_time, connectivity_demands):
 	"""
 	Given a DCP problem instance:
 		- A directed graph with attribute 'weight' on all edges
 		- A dictionary from (node, time) to existence {True, False}
 		- A list of connectivity demands (source, target, time)
+
+	returns the minimum subgraph that respects the connectivity demands.
+
+	Works by reducing to simple DCP (sDCP).
+	"""
+
+	def transform_DCP_to_sDCP(graph, existence_for_node_time, connectivity_demands):
+		"""
+		Given a DCP instance:
+			- A directed graph with attribute 'weight' on all edges
+			- A dictionary from (node, time) to existence {True, False}
+			- A list of connectivity demands (source, target, time)
+
+		returns a simple DCP (sDCP) instance:
+			- A directed graph
+			- An existence dictionary
+			- A list of connectivity demands, all at different times
+			- A single source node
+			- A single target node
+		"""
+		# To be implemented
+
+	def recover_DCP_solution_from_sDCP_solution(subgraph, source, target):
+		"""
+		Given a solution to an instance of sDCP:
+			- A subgraph of the original graph
+			- A universal source node
+			- A universal target node
+
+		returns the subgraph that is the solution to the original DCP instance.
+		"""
+		# To be implemented
+
+	# Reduce to sDCP
+	return recover_DCP_solution_from_sDCP_solution(
+		transform_DCP_to_sDCP(graph, existence_for_node_time, connectivity_demands)
+	)
+
+
+
+def solve_sDCP_instance(graph, existence_for_node_time, connectivity_demands):
+	"""
+	Given a simple DCP (sDCP) problem instance:
+		- A directed graph with attribute 'weight' on all edges
+		- A dictionary from (node, time) to existence {True, False}
+		- A list of connectivity demands (source, target, time)
 			ASSUMPTION: each demand is at a different time
-			TODO: remove this assumption
 
 	returns a list of edges of minimal weight that satisfies the demands.
 	"""
