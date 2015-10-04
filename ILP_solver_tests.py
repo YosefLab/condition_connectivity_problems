c1223663f48fdbada176e40dfc54f18d328ff374
Jimmy <jimmywu126@gmail.com>
2015-10-03 23:27:42 -0700

    Added test for graphs that are trees.

--- ILP_solver_tests.py
+++ ILP_solver_tests.py
@@ -14,8 +14,7 @@ def test_solve_random_DCP_instance():
 	solve_DCP_instance(graph, existence_for_node_time, connectivity_demands)
 
 
-
-def test_solve_DCP_instance_1(feasible=True):
+def test_solve_path_instance(feasible=True):
 	"""
 	Tests the DCP ILP solver on a simple path at two time points.
 	"""
@@ -41,11 +40,44 @@ def test_solve_DCP_instance_1(feasible=True):
 	solve_DCP_instance(graph, existence_for_node_time, connectivity_demands)
 
 
+def test_solve_tree_instance(multiple_times=True):
+	"""
+	Tests the DCP ILP solver on a directed tree at two time points.
+	"""
+	graph = networkx.DiGraph()
+
+	graph.add_path([1,2,3])
+	graph.add_path([1,2,4])
+	for u,v in graph.edges_iter():
+		graph[u][v]['weight'] = 1
+
+	existence_for_node_time = {
+		(1,1): 1,
+		(2,1): 1,
+		(3,1): 1,
+		(4,1): 1,
+		(1,2): 1,
+		(2,2): 1,
+		(3,2): 1,
+		(4,2): 1
+	}
+
+	connectivity_demands = [(1,3,1), (1,4,2 if multiple_times else 1)]
+
+	solve_DCP_instance(graph, existence_for_node_time, connectivity_demands)
+
 
 
 if __name__ == "__main__":
-	test_solve_DCP_instance_1(feasible=True)
-	test_solve_DCP_instance_1(feasible=False)
+	print('Testing path instance, should be feasible')
+	test_solve_path_instance(feasible=True)
+	print('Testing path instance, should be infeasible')
+	test_solve_path_instance(feasible=False)
+
+	print('Testing tree instance with multiple times')
+	test_solve_tree_instance(multiple_times=True)
+	print('Testing tree instance with single time')
+	test_solve_tree_instance(multiple_times=False)
 
 	test_solve_random_DCP_instance()
 
