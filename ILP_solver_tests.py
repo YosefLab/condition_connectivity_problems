af9db593564850ccf4a6a51b0612101fbb3f67e6
Jimmy <jimmywu126@gmail.com>
2015-10-04 18:32:10 -0700

    Added 'anti-greedy' ILP solver test instance. Increased random test instance size.

--- ILP_solver_tests.py
+++ ILP_solver_tests.py
@@ -7,8 +7,11 @@ from ILP_solver.ILP_solver import *
 import matplotlib.pyplot as pyplot
 
 
-def test_solve_random_DCP_instance():
-	graph, existence_for_node_time, connectivity_demands = create_sample_DCP_instance(node_count=10, tree_count=2, tree_span=7)
+def test_solve_random_instance():
+	"""
+	Tests the DCP ILP solver on a randomly generated instance.
+	"""
+	graph, existence_for_node_time, connectivity_demands = create_sample_DCP_instance(node_count=100, tree_count=5, tree_span=15)
 
 	draw_DCP_instance(graph, existence_for_node_time, connectivity_demands)
 
@@ -76,6 +79,37 @@ def test_solve_tree_instance(multiple_times=True):
 	solve_DCP_instance(graph, existence_for_node_time, connectivity_demands)
 
 
+def test_solve_anti_greedy_instance():
+	"""
+	Tests the DCP ILP solver on a small instance that penalizes greedy behavior.
+	"""
+	print('Testing anti-greedy instance')
+
+	graph = networkx.DiGraph()
+
+	graph.add_edge(1, 2, weight=3)
+	graph.add_edge(1, 3, weight=1)
+	graph.add_edge(3, 4, weight=5)
+	graph.add_edge(4, 2, weight=1)
+
+	existence_for_node_time = {
+		(1,1): 1,
+		(2,1): 1,
+		(3,1): 1,
+		(4,1): 1,
+		(1,2): 0,
+		(2,2): 0,
+		(3,2): 1,
+		(4,2): 1
+	}
+
+	connectivity_demands = [(1,2,1), (3,4,2)]
+
+	solve_DCP_instance(graph, existence_for_node_time, connectivity_demands)
+
+
+
+
 if __name__ == "__main__":
 	tests = [
 		(test_solve_path_instance, {'feasible': True}),
@@ -84,7 +118,9 @@ if __name__ == "__main__":
 		(test_solve_tree_instance, {'multiple_times': True}),
 		(test_solve_tree_instance, {'multiple_times': False}),
 
-		(test_solve_random_DCP_instance, {})
+		(test_solve_random_instance, {}),
+
+		(test_solve_anti_greedy_instance, {}),
 	]
 
 	for test, kwargs in tests:
