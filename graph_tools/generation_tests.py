ab4d83e8775af0f1873cc189134874e1251197fb
Jimmy <jimmywu126@gmail.com>
2015-10-04 17:30:30 -0700

    Generalized graph sizes for weighted graphs.

--- graph_tools/generation_tests.py
+++ graph_tools/generation_tests.py
@@ -8,12 +8,9 @@ from visualization import *
 def test_create_sample_DCP_instance():
 	graph, existence_for_node_time, connectivity_demands = create_sample_DCP_instance(node_count=1000, tree_count=10, tree_span=100)
 
-	print("Testing graph generation. Graph has total weight " + str(graph.size()))
+	print("Testing graph generation. Graph has total weight " + str(graph.size(weight='weight')))
 
 	draw_DCP_instance(graph, existence_for_node_time, connectivity_demands)
-
-
-
 
 
 
